f36955d0b5d366afd9926c2e7b03a1e57fb37d12
isiri <isiri@isiri-System-Product-Name.(none)>
2013-02-21 16:36:58 +0530

    added

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,5 +1,4 @@
 class WebsiteController < ApplicationController
-  layout 'website'
   #skip_before_filter :authenticate_user!
   
   def dashboard
--- db/migrate/20130218091350_create_residence_verification_reports.rb
+++ db/migrate/20130218091350_create_residence_verification_reports.rb
@@ -13,22 +13,22 @@ class CreateResidenceVerificationReports < ActiveRecord::Migration
       t.string :locality
       t.string :surroundings
       t.string :accessibility
-      t.boolean :entrance_motorable
-      t.boolean :add_conf
+      t.boolean :entrance_motorable,:default => false
+      t.boolean :add_conf,:default => false
       t.string :correct_pin
       t.string :prominent_landmark    
       t.string :type_off
       t.string :accomodation
       t.string :accom_others
       t.string :no_of_stories
-      t.boolean :watchman
-      t.boolean :lift
-      t.boolean :security_board
-      t.boolean :app_name_outside_door
+      t.boolean :watchman,:default => false
+      t.boolean :lift,:default => false
+      t.boolean:security_board,:default => false
+      t.boolean :app_name_outside_door,:default => false
       t.string :cars_seen_in_around_res
       t.string :make
       t.string :ext_appearence_of_build
-      t.boolean :car_park
+      t.boolean :car_park,:default => false
       t.string :entry_into_res_permited
       t.string :person_cont_name
       t.string :relationship
@@ -39,7 +39,7 @@ class CreateResidenceVerificationReports < ActiveRecord::Migration
       t.string :appro_size_flat_sqft
       t.string :sq_yards
       t.string :ground
-      t.boolean :domestic_help
+      t.boolean :domestic_help,:default => false
       t.date :date_1
       t.text :remarks
       t.date :date_2          
